712b8a0dd2aa7b137e7554342323e50a05eb8e6e
Daniel Chow <DChow@spartaglobal.com>
2020-01-28 15:24:33 +0000

    allows port 22 for ssh

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "aws_security_group" "app_security_group" {
   vpc_id      = aws_vpc.app_vpc.id
 
   ingress {
-   from_port   = 80
-   to_port     = 80
+   from_port   = 22
+   to_port     = 22
    protocol    = "tcp"
    cidr_blocks = ["0.0.0.0/0"]
  }
